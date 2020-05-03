103d1ed446e59680ccfc3773cdcb43b21720fda0
Valentine <valyuscha2004@gmail.com>
2020-05-03 13:04:36 +0300

    Make scroll for homework list

---
+++ src/server/home-work-links.js
@@ -0,0 +1,9 @@
+export const homeWorkLinks = [
+  {id: 1, name: '1. My Blog', src: 'https://valyuscha.github.io/AtomSpace-Blog/'},
+  {id: 2, name: '2. Dice', src: 'https://valyuscha.github.io/Dice/'},
+  {id: 3, name: '3. Header Menu', src: 'https://valyuscha.github.io/Menu/'},
+  {id: 4, name: '4. Glint', src: 'https://valyuscha.github.io/Glint/'},
+  {id: 5, name: '5. Pictures', src: 'https://valyuscha.github.io/Tree-picture/index'},
+  {id: 6, name: '6. Loaders', src: 'https://valyuscha.github.io/Loaders/dist/'},
+  {id: 7, name: '7. Counter and object', src: 'https://valyuscha.github.io/JS-AtomSpace/src/lessons/lesson1/dist/index.html'}
+]
